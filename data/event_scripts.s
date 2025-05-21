a191893b34824b5e90a6425953640c3c05a90ae7
Ariel A <24759293+aarant@users.noreply.github.com>
2025-05-20 23:41:36 -0400

    fix: ensured "PokéNav" is decapped correctly

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -884,7 +884,7 @@ gText_PlayerWhitedOut::
 
 gText_RegisteredTrainerinPokeNav::
 	.string "Registered {STR_VAR_1} {STR_VAR_2}\n"
-	.string "in the POKéNAV.$"
+	.string "in the POKé{}NAV.$"
 
 gText_ComeBackWithSecretPower::
 	.string "Do you know the TM SECRET POWER?\p"
